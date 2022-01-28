ba45c4081beb2ad63a67b5895587823063361c0d
Xharles <charlesnzubechukwu000@gmail.com>
2022-01-28 15:54:09 +0100

    Supported by section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/HomeComponents/Hero";
+import SupportedBy from "../components/HomeComponents/SupportedBy";
 import Layout from "../containers/Layout"
 
 function Home(){
   return(
     <Layout>
       <Hero />
+      <SupportedBy />
     </Layout>
   )
 }
