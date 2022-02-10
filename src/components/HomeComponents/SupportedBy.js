fc47f886588d86f681cccddfc59a27cdd89655f6
Xharles <charlesnzubechukwu000@gmail.com>
2022-02-10 19:54:36 +0100

    links and tablet-size

--- src/components/HomeComponents/SupportedBy.js
+++ src/components/HomeComponents/SupportedBy.js
@@ -3,7 +3,7 @@ import React from "react";
 const supportedByMap = [
   {
     name: "Earthshot Prize",
-    link: "",
+    link: "https://earthshotprize.org/finalists/reeddi-capsules/",
     logo: "/home/awardLogos/Earthshot logo.png",
   },
   {
@@ -12,11 +12,31 @@ const supportedByMap = [
     logo: "/home/awardLogos/ThirdDerivative.png",
   },
   { name: "All on", link: "", logo: "/home/awardLogos/all_on_logo 1.png" },
-  { name: "U of T", link: "", logo: "/home/awardLogos/U of T.png" },
-  { name: "MIT", link: "", logo: "/home/awardLogos/mit_logo 1.png" },
-  { name: "Cisco", link: "", logo: "/home/awardLogos/cisco-2 2.png" },
-  { name: "ASME", link: "", logo: "/home/awardLogos/Asme.png" },
-  { name: "IEEE", link: "", logo: "/home/awardLogos/ieee-1 1.png" },
+  {
+    name: "U of T",
+    link: "https://entrepreneurs.utoronto.ca/announcing-the-2020-rbc-prize-for-innovation-entrepreneurship-winners/",
+    logo: "/home/awardLogos/U of T.png",
+  },
+  {
+    name: "MIT",
+    link: "https://cep.mit.edu/",
+    logo: "/home/awardLogos/mit_logo 1.png",
+  },
+  {
+    name: "Cisco",
+    link: "https://blogs.cisco.com/news/announcing-the-2019-cisco-global-problem-solver-challenge-winners",
+    logo: "/home/awardLogos/cisco-2 2.png",
+  },
+  {
+    name: "ASME",
+    link: "https://thisishardware.org/competition/2020/usa",
+    logo: "/home/awardLogos/Asme.png",
+  },
+  {
+    name: "IEEE",
+    link: "https://empowerabillionlives.org/winners/",
+    logo: "/home/awardLogos/ieee-1 1.png",
+  },
 ];
 
 export default function SupportedBy() {
@@ -26,7 +46,7 @@ export default function SupportedBy() {
       <div className="w-11/12 max-w-7xl flex justify-between ">
         {supportedByMap.map((e) => (
           <a key={e.name} href={e.link}>
-            <img alt={e.name} src={e.logo} className="w-20 h-10" />
+            <img alt={e.name} src={e.logo} className="w-14 lg:w-20 h-7" />
           </a>
         ))}
       </div>
