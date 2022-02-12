70f074d8a02f529dea58ed621cb245c2a3d6d3b6
Xharles <charlesnzubechukwu000@gmail.com>
2022-02-12 13:03:48 +0100

    ico to png

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         start_url: "/",
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
-        icon: "src/images/favicon.ico",
+        icon: "src/images/favicon.png",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
