69d71abae50ee6d1cdf516338aa906f9ae435458
Xharles <charlesnzubechukwu000@gmail.com>
2022-02-15 18:10:15 +0100

    worldclass update

--- src/components/HomeComponents/WorldClassCard.js
+++ src/components/HomeComponents/WorldClassCard.js
@@ -6,7 +6,7 @@ export default function WorldClassCard({ title, text, selected, onClick }) {
       onClick={onClick}
       className={`${
         selected ? "shadow-cstm-2xl" : "shadow-sm"
-      } max-w-[400px] lg:max-w-[520px] py-6 lg:py-8 px-4 lg:px-8 bg-[white] rounded-lg my-20 cursor-pointer`}
+      } max-w-[400px] lg:max-w-[520px] py-6 lg:py-8 px-4 lg:px-8 bg-[white] rounded-lg my-16 md:my-20 cursor-pointer`}
     >
       <div>
         <h2 className="text-xl lg:text-3xl font-semibold">{title}</h2>
--- src/components/HomeComponents/WorldClassInnovation.js
+++ src/components/HomeComponents/WorldClassInnovation.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import WorldClassCard from "./WorldClassCard";
 import WorldClassItemDescriptionCard from "./WorldClassItemDescriptionCard";
+import WorldClassMobileSwitchCard from "./WorldClassMobileSwitchCard";
 
 export default function WorldClassInnovation() {
   const [isCardOne, setIsCardOne] = useState(true);
   const [interval, setinterval] = useState(null);
 
   const setCardInterval = () => {
+    clearInterval(interval);
     const intervalId = setInterval(function () {
       setIsCardOne((prev) => !prev);
     }, 5000);
@@ -14,7 +16,6 @@ export default function WorldClassInnovation() {
   };
 
   const handleClick = (val) => {
-    clearInterval(interval);
     setIsCardOne(val);
     setCardInterval();
   };
@@ -29,12 +30,24 @@ export default function WorldClassInnovation() {
 
   return (
     // pb-8 md:pb-0 added for the absolute below with -bottom
-    <div className="flex flex-col items-center justify-center cstm-bg-green-gradient pt-16 pb-8 md:pb-0">
+    <div className="flex flex-col items-center justify-center cstm-bg-green-gradient pt-16 pb-16 md:pb-0">
       <h3 className="font-matter font-medium text-2xl md:text-4xl mt-10">
         World-Class Innovation
       </h3>
+      <div className="md:hidden flex mt-10">
+        <WorldClassMobileSwitchCard
+          selected={isCardOne}
+          onClick={() => handleClick(true)}
+          title="Reeddi Capsule"
+        />
+        <WorldClassMobileSwitchCard
+          selected={!isCardOne}
+          onClick={() => handleClick(false)}
+          title="Rental MarketPlace"
+        />
+      </div>
       <div className="w-11/12 max-w-7xl flex flex-col md:flex-row">
-        <div className="md:w-1/2">
+        <div className="hidden md:block w-1/2">
           <WorldClassCard
             selected={isCardOne}
             onClick={() => handleClick(true)}
@@ -53,14 +66,30 @@ export default function WorldClassInnovation() {
                 businesses."
           />
         </div>
-        <div className="md:w-1/2 relative flex justify-end lg:justify-center items-center">
+        <div className="md:hidden w-11/2">
+          <WorldClassCard
+            selected
+            title={isCardOne ? "Energy On The Go!" : "Rental Marketplace"}
+            text={
+              isCardOne
+                ? `A clean, affordable, and portable source of electricity that can be
+          used to power homes and businesses appliances and gadgets such as
+          smartphones, MP3 players, digital cameras, e-readers, tablets,
+          laptops, quadcopter drones, CPAP, fans, display monitors, TV etc.`
+                : `A secured and insured digital rental marketplace for accessing
+                essential items and equipment for homes, projects, and
+                businesses.`
+            }
+          />
+        </div>
+        <div className="md:w-1/2 relative flex justify-center items-center">
           {isCardOne ? (
-            <div className="flex">
+            <div className="flex mb-12 md:mb-0">
               <img
                 src="/home/reeddiHand.jpg"
-                className="rounded-md h-64 md:h-72 lg:h-96 xl:h-[480px] mt-8 -mr-16 z-10"
+                className="rounded-md h-64 md:h-72 lg:h-96 xl:h-[480px] mt-8 -mr-8 md:-mr-16 md:z-10"
               />
-              <div className="rounded-md flex items-end bg-[#9efad1] pt- pl-10 pr-5">
+              <div className="rounded-md flex items-end bg-[#9efad1] md:pl-10 pr-5">
                 <img
                   src="/home/girlBagReeddi.jpg"
                   className="h-64 md:h-72 lg:h-96  xl:h-[480px]"
@@ -73,15 +102,15 @@ export default function WorldClassInnovation() {
               />
             </div>
           ) : (
-            <div className="flex mb-10">
+            <div className="flex mb-12 md:mb-10">
               <div className="flex flex-col -mr-16 ">
                 <img
                   src="/home/camera.png"
-                  className="rounded-md h-64 md:h-72 lg:h-96 xl:h-[200px] mt-6 mb-2 z-10"
+                  className="rounded-md h-32 md:h-72 lg:h-96 xl:h-[200px] mt-6 mb-2 z-10"
                 />
                 <img
                   src="/home/drone.png"
-                  className="rounded-md h-64 md:h-72 lg:h-96 xl:h-[200px] mt-2 mb-4 z-10"
+                  className="rounded-md h-32 md:h-72 lg:h-96 xl:h-[200px] mt-2 mb-4 z-10"
                 />
               </div>
 
